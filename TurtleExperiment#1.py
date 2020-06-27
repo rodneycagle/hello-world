54c6fd2c307b7f9ccb49c44c750f69b2861d957b
Phillip Cagle <phillipcagle47@gmail.com>
2020-06-27 12:34:02 -0500

    Phil's first Python commit

---
+++ TurtleExperiment#1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 26 15:08:32 2020
+
+@author: phil
+"""
+
+import turtle
+turtle.bye()
+t   = turtle.getturtle()
+t.color("green","blue")
+t.begin_fill()
+
+for x in range(4):
+    t.left(180)
+    t.forward(100)
+    t.left(90)
+    t.forward(100)
+    t.left(180)
+    t.end_fill()
