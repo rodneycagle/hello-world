f42c14fb817b3b5d7cc63e2140ee5301d0d514d5
Phillip Cagle <phillipcagle47@gmail.com>
2020-06-28 20:48:11 -0500

    added three new programs

---
+++ Square.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 28 17:27:35 2020
+
+@author: cagle_restricted
+"""
+
+
+import turtle
+turtle.bye()
+t   = turtle.getturtle()
+t.color("green", "red")
+
+t.width(5)
+size = int(turtle.numinput("Square Size","What size square do you want?",300,100,560))
+t.penup()
+t.left(270)
+t.forward(size/2)
+t.left(270)
+t.forward(size/2)
+t.left(180)
+t.pendown()
+t.begin_fill()
+for x in range(4):
+    t.forward(size)
+    t.left(90)
+    
+    
+t.end_fill()
